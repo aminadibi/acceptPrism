d5dd5a7eb4c0880d5d41335e8ac4f408807807a2
Adibi <maadibi@ead.ubc.ca>
2020-02-18 15:05:30 -0800

    removed all redis auth for aws testing

--- R/prism_server.R
+++ R/prism_server.R
@@ -9,10 +9,6 @@ get_my_name<-function()
 
 thisSession<-new.env()
 
-thisSession$redis_connection_status<-0
-
-thisSession$REDIS_ADDRESS="prism.resp.core.ubc.ca"
-thisSession$REDIS_PORT <- 3001
 
 thisSession$MODE_REQUIRE_API_KEY=TRUE;
 thisSession$MODE_REQUIRE_SESSION=FALSE;
@@ -24,15 +20,6 @@ thisSession$LONG_RUN_STATUS_ERROR<- -1
 
 thisSession$MODEL_DESCRIPTION<-paste0("This is ",get_my_name()," - PRISM enabled!")
 thisSession$MODEL_VERSION<-paste(packageVersion(get_my_name()))
-
-connect_redis_prism <- function (){
-  if(thisSession$redis_connection_status==0)
-  {
-    rredis::redisConnect(host = thisSession$REDIS_ADDRESS, port = thisSession$REDIS_PORT, password = "H1Uf5o6326n6C2276m727cU82O")
-    thisSession$redis_connection_status<-1
-  }
-}
-
 
 
 
@@ -60,14 +47,6 @@ gateway<-function(...)
   if(is.null(api_key)) api_key=""
   
   if(func!="test") check_access(api_key,session_id,func)
-  
-  #try(
-  #{
-  set_redis_var(paste("RT:LastModelCall:",api_key,sep=""),get_my_name())
-  set_redis_var(paste("RT:LastCallTime:",api_key,sep=""),Sys.time())
-  rredis::redisIncr(paste("RT:CallCount:",api_key,sep=""))
-  rredis::redisRPush(paste("RT:CallTimes:",api_key,sep=""),Sys.time())
-  #})
   
   session_id<<-session_id
   
@@ -101,48 +80,6 @@ prism_model_run<-function(model_input=NULL)
 
 
 
-
-
-
-save_session<-function(session_id)
-{
-  if(!thisSession$MODE_REQUIRE_SESSION_DATA) return()
-  connect_redis_prism()
-  e<-new.env()
-  for(nm in names(globalenv()))
-  {
-    if(typeof(globalenv()[[nm]])!='closure')
-    {
-      e[[nm]]<-globalenv()[[nm]]
-    }
-  }
-  rredis::redisSet(paste(session_id,"env",sep=":"),e)
-}
-
-
-
-
-
-
-restore_session<-function(session_id)
-{
-  if(!thisSession$MODE_REQUIRE_SESSION_DATA) return()
-  connect_redis_prism()
-  e<-rredis::redisGet(paste(session_id,"env",sep=":"))
-  for(nm in names(e))
-  {
-    if(typeof(e[[nm]])!='closure')
-    {
-      .GlobalEnv[[nm]]<-e[[nm]]
-    }
-  }
-}
-
-
-
-
-
-
 #In API-based use without session ids this might seem a bit reduntant (it will not be required). But still good to check model availability
 connect_to_model<-function(api_key="")
 {
@@ -159,14 +96,7 @@ connect_to_model<-function(api_key="")
       return(out)
     }
   }
-  
-  if(thisSession$MODE_REQUIRE_SESSION)
-  {
-    session_id<-generate_session_id()
-    set_redis_var(session_id,value = model_name)
-    out$session_id<-session_id
-  }
-  
+
   out$version<-thisSession$MODEL_VERSION
   out$description<-thisSession$MODEL_DESCRIPTION
   return(out)
@@ -174,53 +104,11 @@ connect_to_model<-function(api_key="")
 
 
 
-
-
-
-disconnect_from_model<-function()
-{
-  if(!is.null(session_id) && session_id!="")
-  {
-    connect_redis_prism()
-    keys<-rredis::redisKeys(pattern = paste(session_id,"*",sep=""))
-    rredis::redisDelete(keys)
-    #To prevent recording of this session environment by the calling gateway.
-    thisSession$MODE_REQUIRE_SESSION_DATA<-FALSE
-    return(TRUE)
-  }
-  else
-  {
-    warning("This was not a sessioned connection. Nothing to disconnet.")
-    return(FALSE)
-  }
-}
-
-
-
-
-
-
-
-
-
 #Checks if the submitted api_key has the privilge to access the model. 
-#Currently only authenticates based on api_key
+#Currently only authenticates based on api_key. * Disabled for test on aws
 check_access<-function(api_key="", session_id="", func=NULL)
 {
-  if(thisSession$MODE_REQUIRE_API_KEY==FALSE) return(TRUE)
-  if(api_key=="") stop("ERROR: API key is required.")
-  
-  #try({
-  #set_redis_var(paste("MODEL_ACCESS/",get_my_name(),":",api_key,sep=""),"kooni")
-  x<-get_redis_var(paste("MT:Access:",get_my_name(),":",api_key,sep=""))
-  if(is.null(x)) stop("ERROR: Unauthorized access.")
-  if(is.numeric(x))
-  {
-    if(x>0) return(TRUE)
-    stop(paste("Access denied - code:",x))
-  }
-  stop(paste("Access denied:",x))
-  #})
+ return(TRUE)
 }
 
 
@@ -234,89 +122,6 @@ generate_session_id<-function()
   id<-paste(c(sample(letters,1) , sample(c(letters,0:9),9,TRUE)),collapse="")
   return(id)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-#' @export
-model_run.long<-function(input)
-{
-  if(is.null(session_id) || session_id=="")
-    stop("Error: long run is not available as this is a session-less connection")
-  
-  key<-paste(session_id,"status",sep=":")
-  
-  if(get_redis_var(key))
-  {
-    #There is already a job for this session in the que!
-    return(FALSE)
-  }
-  else
-    set_redis_var(key,0)
-  
-  return(TRUE)
-}
-
-
-
-
-
-
-
-
-
-#' @export
-prism_check_run_progress<-function()
-{
-  if(is.null(session_id) || session_id=="")
-    stop("Error: long run is not available as this is a session-less connection")
-  
-  key<-paste(session_id,"status",sep=":")
-  
-  val<-get_redis_var(key)
-  
-  if(val)
-  {
-    return(val)
-  }
-  else
-    return(FALSE)
-}
-
-
-
-
-
-
-
-
-
-
-
-#This function is called by model code in a different R process. sesssion_infor should be passed from one process to another.
-#' @export
-prism_set_run_progress<-function(value)
-{
-  if(is.null(session_id) || session_id=="")
-    stop("Error: long run is not available as this is a session-less connection")
-  
-  key<-paste(session_id,"status",sep=":")
-  
-  set_redis_var(key,value)
-}
-
-
-
-
-
 
 
 
@@ -337,35 +142,6 @@ prism_get_output_structure<-function()
   )
   return(out)
 }
-
-
-
-####################Redis example
-
-set_redis_var<-function(variable,value)
-{
-  #TODO: connect should not be in these functions as it will cause multiple connection attempts!
-  connect_redis_prism()
-  rredis::redisSet(variable,value)
-  return(TRUE)
-}
-
-
-get_redis_var<-function(variable)
-{
-  connect_redis_prism()
-  x<-rredis::redisGet(variable)
-  return(x)
-}
-
-
-
-delete_redis_var<-function(variable)
-{
-  connect_redis_prism()
-  rredis::redisDelete(variable)
-}
-
 
 
 
@@ -410,17 +186,3 @@ get_var<-function(variable)
 
 
 
-
-
-#######################################LONG RUN functions###############
-#This is just a placeholder. These functions should not be here!
-
-#browses through rredis for all <session_id:staus,0> pairs and call model run
-prism_patrol<-function()
-  
-  
-  
-  model_run.long<-function(session_id)
-  {
-    
-  }
